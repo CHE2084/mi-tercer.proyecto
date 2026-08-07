d9dd36a63cacbaf649617734cce07d93ca6cdba7
CHE2084 <carcamojose2404@gmail.com>
2026-08-07 14:46:52 -0600

    Add files via upload

---
+++ Ejercicio3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+int resultado;
+int numero1;
+int numero2;
+
+printf("dame el numero 1:\n");
+scanf("%i",&numero1);
+printf("dame el numero 2:\n");
+scanf("%i",&numero2);
+resultado= numero1 / numero2;
+printf("%i", resultado);
+
+}
